126f53af1c01166247009d332b57c903e2ffde08
Serge <32168282+SergeBregliano@users.noreply.github.com>
2024-07-14 12:28:00 +0200

    Initial Release

---
+++ readme.md
@@ -0,0 +1,32 @@
+# Portainer Server
+
+Container [Portainer Server](https://www.portainer.io/) permettant de contrôler une instance [Docker](https://www.docker.com/) locale.
+
+Peut être utilisé avec [des agents](https://github.com/SergeBregliano/docker-portainer-agent).
+
+## Sécurité
+
+L'installation d'un serveur Portainer ne doit pas être exposée au public. L'accès au serveur doit être faite soit sur un réseau local, soit au travers d'un tunnel SSH.
+
+## Installation
+
+Copie des sources dans le répertoire ``portainer_server`` :
+
+```shell
+git clone https://github.com/SergeBregliano/docker-portainer-server.git portainer_agent
+```
+
+Lancer le système de mise à jour :
+
+```shell
+sh ./updateStack.sh
+```
+
+### Connexion au serveur Portainer
+
+Le port exposé par le serveur Portainer est le **9443**.
+
+```http
+https://localhost:9443/
+```
+
---
+++ updateStack.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Use of docker-compose or docker compose
+DOCKERCOMPOSE=docker-compose
+if !(type docker-compose) ; then
+    DOCKERCOMPOSE='docker compose'
+    echo "Will use docker compose to replace docker-compose command."
+fi
+
+# Update the stack
+echo 'Updating configuration files'
+echo '============================'
+git pull https://github.com/SergeBregliano/docker-portainer-server.git main
+echo ''
+
+# Update images
+echo 'Downloading images'
+echo '====================='
+$DOCKERCOMPOSE pull
+echo ''
+
+# Stop last stack, delete it, and start the new one
+echo 'Shutting down servers'
+echo '====================='
+$DOCKERCOMPOSE down
+echo ''
+
+echo 'Deleting images'
+echo '==============='
+$DOCKERCOMPOSE rm -f
+echo ''
+
+echo 'Start new images'
+echo '================'
+$DOCKERCOMPOSE up -d --remove-orphans
+echo ''
+
+echo 'Your stack has been correctly updated.'
+echo ''
